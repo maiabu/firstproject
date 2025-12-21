d76f0e40f7b1b28ec5ebe35e12a143ac3857b670
maiabu <MaiAbu_222313858@gi.edu.eg>
2025-12-21 08:27:39 +0200

    category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Resources\categoryResource;
+
+class CategoryController extends Controller
+{
+    public function store(Request $request){
+        $user=$request->user();
+        if(!$user){
+            return response()->json([
+                'status'=> false,
+                'message'=>[
+                    'ar'=>'يجب تسجل الدخول',
+                    'en'=>'Unauthenticated'
+                ]
+                ]);
+        }
+        
+        $validate = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:2000'],
+             'cover'   =>['required','image','mimes:jpg,png,jpeg,gif','max:2024'],
+        ]);
+
+        $category =Category::create($validate);
+
+         $category ->addMediaFromRequest('cover')->toMediaCollection('cover');
+
+         return response()->json([
+            'status' => true,
+            'message' => [
+                'ar' => 'تم بنجاح',
+                'en' => 'succssefully'
+
+            ],
+            'data' => [
+                'id'  => $category->id,
+                'name' => $category->name,
+                'description' => $category->description,
+                 'cover' => $category->cover_url
+            ],
+            'data'=> new CategoryResource($category)
+            ]);
+}
+
+    public function destroy(Request $request, $id)
+    {
+
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'ar' => 'يجب تسجيل الدخول',
+                    'en' => 'Unauthenticated'
+                ]
+            ]);
+        }
+
+
+        $category= Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'ar' => 'غير موجود',
+                    'en' => 'not found'
+                ]
+            ]);
+        }
+
+        $category->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => [
+                'ar' => 'تم الحذف بنجاح',
+                'en' => 'deleted successfuly'
+            ]
+        ]);
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+
+        $user = $request->user();
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'ar' => 'يجيب تسجيل الدخول',
+                    'en' => 'Unauthenticated'
+                ]
+
+            ]);
+        }
+
+        $category = Category::findOrFail($id);
+
+        if (!$category) {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'ar' => 'غير موجود',
+                    'en' => 'not found'
+                ]
+            ]);
+        }
+
+      $validate = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:2000'],
+        ]);  
+        
+        $category->update($validate);
+
+        return response()->json([
+            'status' => true,
+            'message' => [
+                'ar' => 'تم بنجاح',
+                'en' => 'update successfuly',
+            ]
+        ]);
+}
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\HotelController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->prefix('hotels')->group(function () {
+    Route::post('add', [HotelController::class, 'store']);
+    Route::delete('/delete/{id}', [HotelController::class, 'destroy']);
+    Route::post('update/{id}', [HotelController::class, 'update']);
+});
+
+Route::middleware('auth:sanctum')->prefix('categories')->group(function () {
+    Route::post('add', [CategoryController::class, 'store']);
+    Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
+    Route::post('update/{id}', [CategoryController::class, 'update']);
+});
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
